15fd0b86b34350a9d1c816dab069763b0c4b6d8d
Harsh Vador <harsh.vador@somaiya.edu>
2024-02-21 15:43:28 +0530

    remove ingestion change

--- openmetadata-ui/src/main/resources/ui/src/rest/ingestionPipelineAPI.ts
+++ openmetadata-ui/src/main/resources/ui/src/rest/ingestionPipelineAPI.ts
@@ -43,7 +43,7 @@ export const getIngestionPipelineByFqn = async (
   params?: ListParams
 ) => {
   const response = await APIClient.get<IngestionPipeline>(
-    `/services/ingestionPipelin/name/${getEncodedFqn(fqn)}`,
+    `/services/ingestionPipelines/name/${getEncodedFqn(fqn)}`,
     { params }
   );
 
